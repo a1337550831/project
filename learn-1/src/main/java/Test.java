1ccc458ca4c3ae0de0f30a7675c613c8589d73c4
sunzhuangzhuang <1337550831@qq.com>
2020-05-21 14:56:32 +0800

    learn-1

---
+++ learn-1/src/main/java/Test.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello word");
+        List<String> stringList = new ArrayList<String>();
+
+
+    }
+
+}
